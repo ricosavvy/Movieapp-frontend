360eaae87ff3f7a1601b5e090ee00fa4c8bac92b
Patrick Lungu <patricklungu99@gmail.com>
2023-04-08 21:54:20 +0200

    Another Fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,19 +7,6 @@ import './pages.css'
 
 const Home = () => {
 
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     'X-RapidAPI-Key': '180213137emshf24cf18cd44d128p1d4440jsn7b38e5195953',
-  //     'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-  //   }
-  // };
-  
-  // fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=t', options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
-
   return (
     <>
       <div className='Hero_Section'>
