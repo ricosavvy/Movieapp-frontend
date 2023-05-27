1006254b6b8e1247f8f2a7a562a856886ceaf640
CLEMENTINA NIYINAGIRA <118799691+niyinagira@users.noreply.github.com>
2023-05-27 19:23:56 +0000

    update

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './ContactUs.css';
 import img13 from '../assets/img/img13.jpeg' 
-
- const developers = [
+import img14 from '../assets/img/img14.jpeg'
+import img10 from '../assets/img/img10.jpeg'
+import img11 from '../assets/img/img11.jpeg'
+import img12 from '../assets/img/img12.jpeg'
+import img9 from '../assets/img/img9.jpeg'
+ 
+const developers = [
       {
         id: 1,
         name: 'CLEMENTINA NIYINAGIRA',
@@ -13,31 +18,31 @@ import img13 from '../assets/img/img13.jpeg'
         id: 2,
         name: 'PATRICK LUNGU',
         email: 'patricklungu99@gmail.com',
-        image: 'img11.jpeg',
+        image: `${img11}`,
       },
       {
         id: 3,
         name: 'IVWANAJI SIMBULE',
         email: 'developer3@movieapp.com',
-        image: 'img10.jpeg',
+        image: `${img10}`,
       },
       {
         id: 4,
         name: 'WAZA BANDA',
         email: '1wazabanda@gmail.com',
-        image: 'img12.jpeg',
+        image: `${img12}`,
       },
       {
         id: 5,
         name: 'TWAMBO',
         email: 'Tlcheeba04@gmail.com',
-        image: 'img9.jpeg',
+        image: `${img9}`,
       },
       {
         id: 6,
         name: 'TOM CHILESHI',
         email: 'tomchileshe191@gmail.com',
-        image: 'img14.jpeg',
+        image: `${img14}`,
       },
     ];
      const ContactUs = () => {
