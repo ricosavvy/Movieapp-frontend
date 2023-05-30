d4ba2bc1db6dd497b60961e0e609acffa8780cfa
niyinagira <118799691+niyinagira@users.noreply.github.com>
2023-05-30 17:09:17 +0200

    changes

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,60 +1,135 @@
 import React from 'react';
 import './ContactUs.css';
+import img13 from '../assets/img/img13.jpeg' 
+import img14 from '../assets/img/img14.jpeg'
+import img10 from '../assets/img/img10.jpeg'
+import img11 from '../assets/img/img11.jpeg'
+import img12 from '../assets/img/img12.jpeg'
+import img9 from '../assets/img/img9.jpeg'
+import { useState } from 'react'
 
- const developers = [
+const developers = [
       {
         id: 1,
         name: 'CLEMENTINA NIYINAGIRA',
         email: 'clementinaniyinagira03@gmail.com',
-        image: 'img13.jpeg',
+        image: `${img13}`,
       },
       {
         id: 2,
         name: 'PATRICK LUNGU',
         email: 'patricklungu99@gmail.com',
-        image: 'img11.jpeg',
+        image: `${img11}`,
       },
       {
         id: 3,
         name: 'IVWANAJI SIMBULE',
         email: 'developer3@movieapp.com',
-        image: 'img10.jpeg',
+        image: `${img10}`,
       },
       {
         id: 4,
         name: 'WAZA BANDA',
         email: '1wazabanda@gmail.com',
-        image: 'img12.jpeg',
+        image: `${img12}`,
       },
       {
         id: 5,
         name: 'TWAMBO',
         email: 'Tlcheeba04@gmail.com',
-        image: 'img9.jpeg',
+        image: `${img9}`,
       },
       {
         id: 6,
         name: 'TOM CHILESHI',
         email: 'tomchileshe191@gmail.com',
-        image: 'img14.jpeg',
+        image: `${img14}`,
       },
-    ];
-     const ContactUs = () => {
-  return (
-    <div className="contact-us">
-      <h1>Contact Us</h1>
-      <div className="developers">
-        {developers.map((developer, index) => (
-          <div className="developer" key={index}>
-            <img src={developer.image} id='dev_img' alt={developer.name} />
-            <div className="reflection">
-              <h2>{developer.name}</h2>
-              <p>Email: {developer.email}</p>
+    ]
+    
+    const ContactUs = () => {
+      const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+      });
+    
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission here
+        console.log(formData);
+      };
+    
+      return (
+        <div className="contact-us">
+          <h1 className="header">Contact Us</h1>
+    
+          <div className="developers-section">
+            <h2 className="subheader">Meet Our Developers</h2>
+            <div className="developer-list">
+              {developers.map((developer) => (
+                <div key={developer.id} className="developer-card">
+                  <img
+                    src={developer.image}
+                    alt={developer.name}
+                    className="developer-image"
+                  />
+                  <h3 className="developer-name">{developer.name}</h3>
+                  <p className="developer-email">{developer.email}</p>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
-      </div>
-    </div>
-    );
-  };
-export default ContactUs;
+    
+          <div className="contact-form-section">
+            <h2 className="subheader">Get in Touch With Us</h2>
+            <form className="contact-form" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+              />
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                value={formData.subject}
+                onChange={handleInputChange}
+                required
+              />
+              <textarea
+                name="message"
+                placeholder="Your Message"
+                value={formData.message}
+                onChange={handleInputChange}
+                required
+              ></textarea>
+              <button type="submit">Send Message</button>
+            </form>
+          </div>
+        </div>
+      );
+    };
+  export default ContactUs;
+  
+      
